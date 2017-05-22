09b87f9c641ac7db6d3b4a7c9503600fa26cfdd2
AllenZhenf <913325695@qq.com>
2017-05-22 15:11:21 +0800

    11111

--- test/com/shenzhenfeng/util/JunitFlowTest.java
+++ test/com/shenzhenfeng/util/JunitFlowTest.java
@@ -10,6 +10,18 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 public class JunitFlowTest {
+	
+	/**
+	 *  @Test: 将一个 普通的方法修饰成为一个测试方法
+	 *  @BeforeClass: 他会在所有的方法运行前被执行，static修饰
+	 *  @AfterClass 他会在所有方法运行结束后被执行，static修饰
+	 *  @Before：会在每一个测试方法被运行前执行一次
+	 *  @After 会在每个测试方法被运行后执行一次
+	 *  @Ignore 所修饰的测试方法会被测试运行器忽略
+	 *  @RunWith: 可以修改测试运行器 org.junit.runner.Runner
+	 *  
+	 */
+
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
